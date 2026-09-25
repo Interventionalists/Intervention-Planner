1dc348b689bae9b8c3985cb7cd45bf14e8ba9724
DylanDudley <dudleyd8828@my.uwstout.edu>
2026-09-25 09:05:18 -0500

    SCRUM-43: Comments on files

--- PUnit/test_error_codes.py
+++ PUnit/test_error_codes.py
@@ -1,7 +1,17 @@
+# ------------------------------------------------------------------------------
+#
+# Name:        test_error_codes.py
+# Purpose:     A simple test file to check the error code definitions.
+# Author:      Dudley D. (09/25/2026)
+#
+# ------------------------------------------------------------------------------
+
 import pytest
 
+# Loads the error definitions from the backend.error module to test them.
 from backend.error import ValidationError, ERROR_CODES
 
+# Generic test to make sure the ValidationError class is defined and can be instantiated.
 def test_validation_error_has_expected_code_and_payload():
     err = ValidationError("Missing required field", details={"field": "name"})
 
